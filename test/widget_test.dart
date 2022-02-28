83d91ab4fe06c3f479c1b104dc71e3260bc627d7
yymin1022 <yymin1022@gmail.com>
2022-02-28 21:11:57 +0900

    Fixed Test Code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:syno_downlaodstation/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const DSApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
