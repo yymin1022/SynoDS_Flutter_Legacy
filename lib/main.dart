7daace031b7d25d91a39707fbfc5cea6294c123d
yymin1022 <yymin1022@gmail.com>
2022-03-05 21:21:02 +0900

    Update main.dart : Add Floating Action Button

--- lib/main.dart
+++ lib/main.dart
@@ -15,30 +15,35 @@ class DSApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         home: Scaffold(
-            appBar: AppBar(
-              title: Text("SynoDS"),
-              actions: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.settings),
-                  tooltip: "Settings",
-                  onPressed: () => {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.info),
-                  tooltip: "Info",
-                  onPressed: () => {},
-                )
-              ]
+          appBar: AppBar(title: Text("SynoDS"), actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.settings),
+              tooltip: "Settings",
+              onPressed: () => {},
             ),
-            body: SafeArea(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
+            IconButton(
+              icon: Icon(Icons.info),
+              tooltip: "Info",
+              onPressed: () => {},
+            )
+          ]),
+          body: SafeArea(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
                   Container(child: const StatusView()),
                   Divider(thickness: 1, height: 1, color: Color(0xFFEEEEEE)),
                   Container(child: const TaskView())
-                ]))));
+                ]),
+          ),
+          floatingActionButton: FloatingActionButton.extended(
+            onPressed: () {},
+            label: Text(""),
+            icon: const Icon(Icons.add_rounded),
+            backgroundColor: Colors.blue,
+          ),
+        ));
   }
 }
 
@@ -66,12 +71,11 @@ class _StatusViewState extends State<StatusView> {
                   width: 160,
                   margin: EdgeInsets.fromLTRB(20, 20, 10, 20),
                   decoration: BoxDecoration(
-                    border: Border.all(
+                      border: Border.all(
                         color: Color(0xFFDDDDDD),
                         width: 2,
-                    ),
-                    borderRadius: BorderRadius.circular(10)
-                  ),
+                      ),
+                      borderRadius: BorderRadius.circular(10)),
                   child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
@@ -84,12 +88,11 @@ class _StatusViewState extends State<StatusView> {
                   width: 160,
                   margin: EdgeInsets.fromLTRB(10, 20, 20, 20),
                   decoration: BoxDecoration(
-                    border: Border.all(
+                      border: Border.all(
                         color: Color(0xFFDDDDDD),
                         width: 2,
-                    ),
-                    borderRadius: BorderRadius.circular(10)
-                  ),
+                      ),
+                      borderRadius: BorderRadius.circular(10)),
                   child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
@@ -114,17 +117,15 @@ class _TaskViewState extends State<TaskView> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 350,
-      
-      child: Column(
-        children: <Widget>[
-          TaskViewItem(new TaskData("Test Task 1", "TaskID", 12.5)),
-          TaskViewItem(new TaskData("Test Task 2", "TaskID", 22.3)),
-          TaskViewItem(new TaskData("Test Task 3", "TaskID", 100)),
-          TaskViewItem(new TaskData("Test Task 4", "TaskID", 0.5)),
-        ],
-      )
-    );
+        width: 350,
+        child: Column(
+          children: <Widget>[
+            TaskViewItem(new TaskData("Test Task 1", "TaskID", 12.5)),
+            TaskViewItem(new TaskData("Test Task 2", "TaskID", 22.3)),
+            TaskViewItem(new TaskData("Test Task 3", "TaskID", 100)),
+            TaskViewItem(new TaskData("Test Task 4", "TaskID", 0.5)),
+          ],
+        ));
   }
 }
 
