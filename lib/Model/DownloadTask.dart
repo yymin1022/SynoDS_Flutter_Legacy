cf488887184639d476105a1a2efb702b2c03a280
yymin1022 <yymin1022@gmail.com>
2023-03-10 03:14:15 +0900

    Update DownloadTask.dart : Added Basic Variables for Class

--- lib/Model/DownloadTask.dart
+++ lib/Model/DownloadTask.dart
@@ -1,3 +1,30 @@
+import 'dart:ffi';
+
 class DownloadTask {
-  
+  String? _id;
+  String? _type;
+  String? _username;
+  String? _title;
+  String? _size;
+  String? _status;
+  String? _status_extra;
+  DownloadTaskDetail? _detail;
+  List<DownloadTaskFile>? _file;
+}
+
+class DownloadTaskDetail {
+  int? _connectedLeechers;
+  int? _connectedSedders;
+  int? _totalPeers;
+  String? _create_time;
+  String? _destination;
+  String? _priority;
+  String? _uri;
+}
+
+class DownloadTaskFile {
+  String? _filename;
+  String? _priority;
+  String? _size;
+  String? _sizeDownloaded;
 }
